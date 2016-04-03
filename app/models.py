0a65f6038ab3c35196c9e45fd05894ebe4aade85
Heath <Heath.kang@foxmail.com>
2016-04-03 19:44:31 +0800

    完成zhifufake 导航条

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ import hashlib
 from flask import request
 from . import db
 from flask.ext.login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
 
 '''
@@ -31,4 +32,37 @@ class Post(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
+    author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+
+
+
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(64),unique=True,index=True)
+    username = db.Column(db.String(64),unique=True,index=True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean,default=False)
+    posts = db.relationship('Post',backref='author',lazy='dynamic')
+
+    @property
+    def password(self):
+        def password(self):
+            raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self,password_hash,password)
+
+
+
+
+
+
+
 
